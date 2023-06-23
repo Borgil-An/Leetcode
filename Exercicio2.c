cf95417cddb4db6119bbf768f64b16e589ac44f6
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2023-06-23 18:23:04 -0300

    Update Exercicio2.c

--- Exercicio2.c
+++ Exercicio2.c
@@ -1,1 +1,12 @@
+// Somar os numeros do array com o anterior
 
+int* runningSum(int* nums, int numsSize, int* returnSize){
+  int comp=0;
+*returnSize = numsSize; 
+    
+for(int i=1;i<numsSize;i++)
+    {
+        nums[i]+=nums[i-1];
+    }
+    return nums;
+}
