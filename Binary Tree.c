c0a1e5930190bcc03ef2565c23cb5c16a5176d94
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2023-08-18 01:39:05 -0300

    Create Binary Tree.c

---
+++ Binary Tree.c
@@ -0,0 +1,41 @@
+struct TreeNode* create(int val) {
+    struct TreeNode *node = malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    
+    return node;
+}
+
+void insertLeft(struct TreeNode* root, int val) {
+    root->left = create(val);
+}
+
+void insertRight(struct TreeNode* root, int val) {
+    root->right = create(val);
+}
+
+void inorderTraversalHelper(struct TreeNode* root, int* result, int* index) {
+    if (root == NULL) {
+        return;
+    }
+
+    inorderTraversalHelper(root->left, result, index);
+    result[(*index)++] = root->val;
+    inorderTraversalHelper(root->right, result, index);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    *returnSize = 0;
+    if (root == NULL) {
+        return NULL;
+    }
+
+    int *result = malloc(1000 * sizeof(int)); // Assuming a maximum of 1000 nodes
+    int index = 0;
+    
+    inorderTraversalHelper(root, result, &index);
+    *returnSize = index;
+
+    return result;
+}
