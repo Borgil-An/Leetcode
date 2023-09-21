5d7876ab44bbd71df557d6c9d6f0c0abf0e58f24
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2023-09-20 21:04:18 -0300

    Update and rename Fizz Buzz to 412.c

--- 412.c
+++ 412.c
@@ -3,7 +3,7 @@ char ** fizzBuzz(int n, int* returnSize){
     char **T = (char**)malloc(sizeof(char*)*n);
     for(int i=0;i<n;i++){
         T[i]=(char*)malloc(9*sizeof(char));
-        sprintf(T[i], "%d", i+1);  // Convert 'i' to char 
+        sprintf(T[i], "%d", i+1); 
         if( (i+1) % 3 == 0 && (i+1) % 5 == 0){T[i] = "FizzBuzz";}
         else if((i+1)%3 == 0){T[i]= "Fizz";}
         else if((i+1)%5 == 0){T[i] = "Buzz";}
