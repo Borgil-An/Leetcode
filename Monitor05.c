3215219d7ffa4ea9a00a81cca2130e6a5d94eaf7
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2023-09-20 22:08:42 -0300

    Update Monitor05.c

--- Monitor05.c
+++ Monitor05.c
@@ -1,1 +1,28 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int targetSum){  
+    if (root == NULL) {
+        return false;
+    }
+    
+    targetSum -= root->val;
+    
+    
+    if (root->left == NULL && root->right == NULL && targetSum == 0) {
+        return true;
+    }
+    
+    
+    bool leftPath = hasPathSum(root->left, targetSum);
+    bool rightPath = hasPathSum(root->right, targetSum);
+    
+   
+    return leftPath || rightPath;
+}
